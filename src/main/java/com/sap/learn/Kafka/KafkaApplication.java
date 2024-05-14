7b84219699e627b19414f9e4e21563fb416bc311
poshakjaiswal <poshakkj@gmail.com>
2024-05-14 08:32:50 +0530

    Kafka

--- src/main/java/com/sap/learn/Kafka/KafkaApplication.java
+++ src/main/java/com/sap/learn/Kafka/KafkaApplication.java
@@ -3,7 +3,9 @@ package com.sap.learn.Kafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sap.learn.Kafka.controller")
+
 public class KafkaApplication {
 
 	public static void main(String[] args) {
